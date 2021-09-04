d87e2938e58d96fe7b25d8e07430bd1c5154627a
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-04 20:48:23 +0530

    Typed the whole custom hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,39 @@
+import { useSnackbar } from "notistack";
 import { useEffect, useState } from "react";
 
-function useLocalStorage() {
-  interface TodosSchema {
-    id: number;
-    todos: string;
-  }
+export interface TodosSchema {
+  index: number;
+  todos: string;
+  completed: boolean;
+}
+export interface UpdateTodoProps {
+  newTodosArr?: TodosSchema[];
+  content?: TodosSchema;
+  index?: number;
+}
 
-  // We might also add the notifications function here only to simplify the code.
+export interface ReturnInterface {
+  todosArray: TodosSchema[];
+  clearTodos: (index?: number) => void;
+  completeTodo: (
+    value: TodosSchema,
+    completeOrNot: boolean,
+    element: HTMLSpanElement,
+    index?: number
+  ) => void;
+  addTodo: (value: TodosSchema, index?: number) => void;
+  deleteTodo: (index: number) => void;
+  updateTodo: (props: UpdateTodoProps) => void;
+  setTodos: React.Dispatch<React.SetStateAction<TodosSchema[]>>;
+}
 
+/**
+ * @description Returns the useLocalStorage hook.
+ * @returns {ReturnInterface} Helper functions to use inside the whole app.
+ */
+
+function useLocalStorage(): ReturnInterface {
+  const { enqueueSnackbar } = useSnackbar();
   const [todosArray, setTodos] = useState<TodosSchema[]>([]);
   let todos: TodosSchema[] = todosArray;
   let localStorageTodos = localStorage.getItem("todos");
@@ -19,11 +45,19 @@ function useLocalStorage() {
     }
   }, [localStorageTodos]);
 
-  useEffect(() => {
+  useEffect((): void => {
     todos = todosArray;
   }, [todosArray]);
 
-  const clearTodos = (index?: number) => {
+  /**
+   * @function
+   * @method
+   * @description Clear's all the todos if no index is passed. If an index is passed, it will clear the todo at the index passed.
+   * @param {number} index - (Optional) Index of the todo to be deleted.
+   * @inner
+   */
+
+  const clearTodos = (index?: number): void => {
     if (index) {
       setTodos(todosArray.splice(index, 1));
       localStorage.setItem("todos", JSON.stringify(todosArray));
@@ -31,9 +65,17 @@ function useLocalStorage() {
       setTodos([]);
       localStorage.setItem("todos", JSON.stringify([]));
     }
+    enqueueSnackbar("Todos Cleared!", {
+      variant: "success",
+    });
   };
 
-  const addTodo = (value: TodosSchema, index?: number) => {
+  /**
+   * @description Adds a todo to the todos array relative to the parameters passed.
+   * @param {TodosSchema} value - The todo to be added.
+   * @param {number} [index] - (Optional) The index to add a todo.
+   */
+  const addTodo = (value: TodosSchema, index?: number): void => {
     if (index) {
       todos?.splice(index, 1, value);
       setTodos(todos);
@@ -43,27 +85,67 @@ function useLocalStorage() {
       setTodos(todos);
       localStorage.setItem("todos", JSON.stringify(todosArray));
     }
+    enqueueSnackbar("Todo added!", {
+      variant: "success",
+    });
   };
 
-  const deleteTodo = (index: number) => {
-    // There might be a better way to do this!
-    todos.splice(index, 1);
-    localStorage.setItem("todos", JSON.stringify(todos));
-    let newArr = localStorage.getItem("todos");
-    if (newArr) {
-      setTodos(JSON.parse(newArr));
+  const completeTodo = (
+    todo: TodosSchema,
+    completeOrNot: boolean,
+    element: HTMLSpanElement,
+    index?: number
+  ): void => {
+    if (completeOrNot) {
+      todo.completed = true;
+      todos[todo.index - 1] = todo;
+      setTodos(todos);
+      localStorage.setItem("todos", JSON.stringify(todosArray));
+      element.innerHTML = todo.todos.strike();
+    } else {
+      todo.completed = false;
+      todos[todo.index - 1] = todo;
+      setTodos(todos);
+      localStorage.setItem("todos", JSON.stringify(todosArray));
+      element.innerHTML = todo.todos;
     }
   };
 
-  interface UpdateTodoProps {
-    newTodosArr?: TodosSchema[];
-    content?: TodosSchema;
-    index?: number;
-  }
+  /**
+   * @method
+   * @function
+   * @description Deletes a todo at the index passed.
+   * @param {number} index - The index of the todo to be deleted.
+   */
 
-  const updateTodo = ({ newTodosArr, content, index }: UpdateTodoProps) => {
+  const deleteTodo = (index: number): void => {
+    todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    let newArr = localStorage.getItem("todos");
+    setTodos(JSON.parse(newArr));
+    enqueueSnackbar("Todo Deleted!", {
+      variant: "error",
+    });
+  };
+
+  /**
+   * @description Updates a todo at a specific index.
+   * @param {UpdateTodoProps} props - The props to be passed to update the todo.
+   * @param {TodosSchema[]} props.newTodosArr - The new todos array to be set.
+   * @param {TodosSchema} props.content - The todo content to be updated.
+   * @param {number} props.index - The index of the todo to be updated.
+   */
+
+  const updateTodo = ({
+    newTodosArr,
+    content,
+    index,
+  }: UpdateTodoProps): void => {
     if (content) {
       todos[index].todos = content.todos;
+      enqueueSnackbar("Todo Updated!", {
+        variant: "success",
+      });
     } else {
       todos = newTodosArr;
     }
@@ -71,7 +153,15 @@ function useLocalStorage() {
     setTodos(todos);
   };
 
-  return { todosArray, clearTodos, addTodo, deleteTodo, updateTodo, setTodos };
+  return {
+    todosArray,
+    clearTodos,
+    addTodo,
+    deleteTodo,
+    updateTodo,
+    setTodos,
+    completeTodo,
+  };
 }
 
 export default useLocalStorage;
