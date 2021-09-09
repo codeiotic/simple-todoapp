8a9258a54585a446830b0c8ea681f264f51591ad
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-09 17:34:50 +0530

    Tweaked a few things

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { SnackbarProvider } from "notistack";
 import { render } from "react-dom";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
+
 render(
   <SnackbarProvider maxSnack={1} autoHideDuration={2000}>
     <App />
