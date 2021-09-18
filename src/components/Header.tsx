96a283c0cd8e65bfef8356a4d9211a709070cfc7
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-18 19:31:04 +0530

    Created the Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import HeaderStyles from "../styles/Header";
+
+const Header = () => {
+  const classNames = HeaderStyles();
+  return (
+    <header className={classNames.header}>
+      <div className={classNames.imgParent}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+          alt="React Logo"
+          className={classNames.img}
+        />
+      </div>
+      <nav>
+        <div className={classNames.linkParent}>
+          <Link to="/sign-up" className={classNames.links}>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#2acafe",
+                height: "2.1rem",
+              }}
+              className={classNames.primaryBtn}
+            >
+              Sign Up
+            </Button>
+          </Link>
+          <Link to="/login" className={classNames.links}>
+            <Button
+              variant="outlined"
+              style={{
+                border: "1px solid white",
+                height: "2.1rem",
+              }}
+            >
+              LogIn
+            </Button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/styles/Header.ts
@@ -0,0 +1,50 @@
+import { Theme, useMediaQuery } from "@material-ui/core";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+
+const HeaderStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      height: "60px",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      backgroundColor: "#002233",
+    },
+    imgParent: {
+      width: "15%",
+      maxWidth: "80px",
+      minWidth: "70px",
+      marginLeft: "40px",
+    },
+    img: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    linkParent: {
+      "&:last-child": {
+        marginRight: "40px",
+      },
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: "30px",
+    },
+    links: {
+      textDecoration: "none",
+      fontSize: "1rem",
+      fontWeight: "lighter",
+    },
+    primaryBtn: {
+      "& .MuiButton-label": {
+        color: "black",
+      },
+    },
+  })
+);
+
+export default HeaderStyles;
