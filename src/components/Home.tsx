2d410c5b362468f56e087cef746d375d97a7f1c0
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-19 21:21:53 +0530

    Created a simple landing page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,15 @@
-const Home = () => {
+import HomeStyles from "../styles/Home";
+
+const Home = (): JSX.Element => {
+  const classNames = HomeStyles();
+
   return (
-    <>
-      <main></main>
-    </>
+    <main className={classNames.parent}>
+      <h1 className={classNames.heading}>Just a simple Todo App</h1>
+      <p className={classNames.ghosted}>
+        Stores all the Todos you need to do in a day!
+      </p>
+    </main>
   );
 };
 
--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -1,6 +1,26 @@
 import { Theme } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-const HomeStyles = makeStyles((theme: Theme) => createStyles({}));
+const HomeStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    parent: {
+      backgroundColor: "#002233",
+      marginTop: "60px",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "start",
+      flexDirection: "column",
+    },
+    heading: {
+      fontSize: "1.7rem",
+      marginTop: "20px",
+    },
+    ghosted: {
+      marginTop: "10px",
+      opacity: 0.5,
+    },
+  })
+);
 
 export default HomeStyles;
