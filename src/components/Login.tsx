f72652dd4ffbb2e360b30fbd5bad634c7be57203
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-23 17:21:06 +0530

    Used `react-form` to prevent extra re-renders

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,134 +1,168 @@
-import { Button, Divider, Switch, TextField } from "@material-ui/core";
+import { Divider, Switch, TextField } from "@material-ui/core";
 import { useSnackbar } from "notistack";
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { supabase } from "../db/supabaseClient";
 import LogInStyles from "../styles/Login";
 import { useHistory } from "react-router-dom";
 import Loader from "react-loader-spinner";
 import UserContext from "../hooks/userContext";
+import userActivity from "../db/userActivity";
+import {
+  UserActivityReturnInterface,
+  UserActivityInputInterface,
+} from "../db/userActivity";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { motion } from "framer-motion";
+import { Button } from "../components/Button";
 
 const LogIn = (): JSX.Element => {
   const classNames = LogInStyles();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [checked, setChecked] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   let location = useHistory();
   const { enqueueSnackbar } = useSnackbar();
   const supabaseUser = supabase.auth.user();
+  const { register, handleSubmit, setValue } =
+    useForm<UserActivityInputInterface>();
 
-  const logInUser = async (e: MouseEvent): Promise<void> => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      let { error, user } = await supabase.auth.signIn({
-        email,
-        password,
-      });
+  const transition = { duration: 0.4, ease: "easeInOut" };
+
+  const onFormSubmit: SubmitHandler<UserActivityInputInterface> = ({
+    email,
+    password,
+  }: UserActivityInputInterface): void => {
+    setLoading(true);
+    userActivity({
+      email: email,
+      password: password,
+      type: "login",
+      finalCallback: (): void => {
+        setLoading(false);
+        location.push("/home");
+      },
+    }).then(({ error, user }: UserActivityReturnInterface): void => {
       if (error) {
         enqueueSnackbar(error.message, { variant: "error" });
+      } else {
+        enqueueSnackbar("Logged in as " + user.email, { variant: "success" });
       }
-      enqueueSnackbar("Logged in as " + user.email, { variant: "success" });
-    } catch (error) {
-    } finally {
-      setLoading(false);
-      location.push("/home");
-    }
+    });
   };
 
   const clear = (): void => {
-    setEmail("");
-    setPassword("");
     setChecked(false);
+    setValue("email", "", {
+      shouldTouch: true,
+      shouldDirty: true,
+    });
+    setValue("password", "", {
+      shouldTouch: true,
+      shouldDirty: true,
+    });
   };
 
   useEffect((): void => {
-    if (supabaseUser?.email) {
+    if (supabaseUser) {
       location.push("/home");
     }
   }, [supabaseUser, location]);
 
   return (
-    <UserContext.Consumer>
-      {(): JSX.Element => {
-        return (
-          <div className={classNames.parent}>
-            <h1 className={classNames.heading}>Log In</h1>
-            <Divider className={classNames.divider} />
-            <form className={classNames.form}>
-              <TextField
-                className={classNames.emailInput}
-                type="email"
-                variant="filled"
-                size="small"
-                label="Email"
-                fullWidth
-                required
-                value={email}
-                onChange={(e: ChangeEvent<HTMLTextAreaElement>): void =>
-                  setEmail(e.target.value)
-                }
-              />
-              <TextField
-                className={classNames.passwordInput}
-                type={checked ? "text" : "password"}
-                variant="filled"
-                size="small"
-                label="Password"
-                required
-                fullWidth
-                value={password}
-                onChange={(e: ChangeEvent<HTMLTextAreaElement>): void =>
-                  setPassword(e.target.value)
-                }
-              />
-              <p className={classNames.showPwd}>Show password</p>
-              <Switch
-                size="small"
-                className={classNames.switch}
-                checked={checked}
-                onChange={(
-                  _: ChangeEvent<HTMLInputElement>,
-                  checked: boolean
-                ): void => setChecked(checked)}
-              />
-              <Divider className={classNames.divider} />
-              <div className={classNames.buttons}>
-                <Button
-                  variant="contained"
-                  type="submit"
-                  className={classNames.primaryButton}
-                  onClick={logInUser}
-                >
-                  {loading ? (
-                    <Loader
-                      type="Oval"
-                      color="#002233"
-                      height="24"
-                      width="42"
-                    />
-                  ) : (
-                    "Login"
-                  )}
-                </Button>
-                <Button
-                  variant="outlined"
-                  type="button"
-                  className={classNames.secondaryButton}
-                  onClick={clear}
-                >
-                  Cancel
-                </Button>
-              </div>
-            </form>
-            <p className={classNames.signUpLink}>
-              Don't have an account? <Link to="/sign-up">Sign Up</Link>
-            </p>
-          </div>
-        );
+    <motion.div
+      exit={{
+        opacity: 0.9,
+        backgroundColor: "#002233",
       }}
-    </UserContext.Consumer>
+      initial={{
+        opacity: 0.9,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={transition}
+    >
+      <UserContext.Consumer>
+        {(): JSX.Element => {
+          return (
+            <div className={classNames.parent}>
+              <h1 className={classNames.heading}>Log In</h1>
+              <Divider className={classNames.divider} />
+              <form
+                className={classNames.form}
+                onSubmit={handleSubmit(onFormSubmit)}
+              >
+                <TextField
+                  className={classNames.emailInput}
+                  type="email"
+                  variant="filled"
+                  size="small"
+                  label="Email"
+                  autoComplete="off"
+                  fullWidth
+                  required
+                  {...register("email", {
+                    required: true,
+                  })}
+                />
+                <TextField
+                  className={classNames.passwordInput}
+                  type={checked ? "text" : "password"}
+                  variant="filled"
+                  size="small"
+                  label="Password"
+                  autoComplete="off"
+                  required
+                  fullWidth
+                  {...register("password", {
+                    required: true,
+                  })}
+                />
+                <p className={classNames.showPwd}>Show password</p>
+                <Switch
+                  size="small"
+                  className={classNames.switch}
+                  checked={checked}
+                  onChange={(
+                    _: ChangeEvent<HTMLInputElement>,
+                    checked: boolean
+                  ): void => setChecked(checked)}
+                />
+                <Divider className={classNames.divider} />
+                <div className={classNames.buttons}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    variantType="primary"
+                  >
+                    {loading ? (
+                      <Loader
+                        type="Oval"
+                        color="#002233"
+                        height="24"
+                        width="42"
+                      />
+                    ) : (
+                      "Login"
+                    )}
+                  </Button>
+                  <Button
+                    variantType="secondary"
+                    type="button"
+                    variant="outlined"
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+              <p className={classNames.signUpLink}>
+                Don't have an account? <Link to="/sign-up">Sign Up</Link>
+              </p>
+            </div>
+          );
+        }}
+      </UserContext.Consumer>
+    </motion.div>
   );
 };
 
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,133 +1,161 @@
-import { Button, Divider, Switch, TextField } from "@material-ui/core";
+import { Divider, Switch, TextField } from "@material-ui/core";
+import { motion } from "framer-motion";
 import { useSnackbar } from "notistack";
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import Loader from "react-loader-spinner";
 import { Link, useHistory } from "react-router-dom";
 import { supabase } from "../db/supabaseClient";
-import UserContext, { UserContextInterface } from "../hooks/userContext";
+import userActivity, {
+  UserActivityInputInterface,
+  UserActivityReturnInterface,
+} from "../db/userActivity";
+import UserContext from "../hooks/userContext";
 import SignUpStyles from "../styles/SignUp";
+import { Button } from "../components/Button";
 
 const SignUp = (): JSX.Element => {
   const classNames = SignUpStyles();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [checked, setChecked] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const location = useHistory();
   const supabaseUser = supabase.auth.user();
-
   const { enqueueSnackbar } = useSnackbar();
+  const { register, handleSubmit, setValue } =
+    useForm<UserActivityInputInterface>();
 
-  const signUpUser = async (e: MouseEvent): Promise<void> => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const { error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-      });
+  const transition = { duration: 0.4, ease: "easeInOut" };
+
+  const onFormSubmit: SubmitHandler<UserActivityInputInterface> = ({
+    email,
+    password,
+  }: UserActivityInputInterface): void => {
+    setLoading(true);
+    console.log(email, password);
+    userActivity({
+      email: email,
+      password: password,
+      type: "signUp",
+      finalCallback: (): void => {
+        setLoading(false);
+        location.push("/home");
+      },
+    }).then(({ error, user }: UserActivityReturnInterface): void => {
       if (error) {
-        enqueueSnackbar(error.message);
+        enqueueSnackbar(error.message, { variant: "error" });
+      } else {
+        enqueueSnackbar("Signed up as " + user.email, { variant: "success" });
       }
-      enqueueSnackbar("Check your email for magic link!");
-    } catch (error) {
-      console.log(error.message);
-    } finally {
-      location.push("/home");
-      setLoading(false);
-    }
+    });
   };
 
   useEffect((): void => {
-    if (supabaseUser?.email) {
+    if (supabaseUser) {
       location.push("/home");
     }
   }, [supabaseUser, location]);
 
   const clear = (): void => {
-    setEmail("");
-    setPassword("");
+    setValue("email", "");
+    setValue("password", "");
     setChecked(false);
   };
 
   return (
-    <UserContext.Consumer>
-      {({ user }: UserContextInterface): JSX.Element => {
-        return (
-          <div className={classNames.parent}>
-            <h1 className={classNames.heading}>Sign Up</h1>
-            <Divider className={classNames.divider} />
-            <form className={classNames.form}>
-              <TextField
-                className={classNames.emailInput}
-                type="email"
-                variant="filled"
-                size="small"
-                label="Email"
-                fullWidth
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                className={classNames.passwordInput}
-                type={checked ? "text" : "password"}
-                variant="filled"
-                size="small"
-                label="Password"
-                required
-                fullWidth
-                value={password}
-                onChange={(e: ChangeEvent<HTMLTextAreaElement>): void =>
-                  setPassword(e.target.value)
-                }
-              />
-              <p className={classNames.showPwd}>Show password</p>
-              <Switch
-                size="small"
-                className={classNames.switch}
-                checked={checked}
-                onChange={(
-                  _: ChangeEvent<HTMLInputElement>,
-                  checked: boolean
-                ): void => setChecked(checked)}
-              />
-              <Divider className={classNames.divider} />
-              <div className={classNames.buttons}>
-                <Button
-                  variant="contained"
-                  type="submit"
-                  className={classNames.primaryButton}
-                  onClick={signUpUser}
-                >
-                  {loading ? (
-                    <Loader
-                      type="Oval"
-                      color="#002233"
-                      height="24"
-                      width="42"
-                    />
-                  ) : (
-                    "Sign Up"
-                  )}
-                </Button>
-                <Button
-                  variant="outlined"
-                  type="button"
-                  className={classNames.secondaryButton}
-                  onClick={clear}
-                >
-                  Cancel
-                </Button>
-              </div>
-            </form>
-            <p className={classNames.signUpLink}>
-              Already have an account? <Link to="/login">Log In</Link>
-            </p>
-          </div>
-        );
+    <motion.div
+      exit={{
+        opacity: 0.9,
+        backgroundColor: "#002233",
       }}
-    </UserContext.Consumer>
+      initial={{
+        opacity: 0.9,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={transition}
+    >
+      <UserContext.Consumer>
+        {(): JSX.Element => {
+          return (
+            <div className={classNames.parent}>
+              <h1 className={classNames.heading}>Sign Up</h1>
+              <Divider className={classNames.divider} />
+              <form
+                className={classNames.form}
+                onSubmit={handleSubmit(onFormSubmit)}
+              >
+                <TextField
+                  className={classNames.emailInput}
+                  type="email"
+                  variant="filled"
+                  size="small"
+                  label="Email"
+                  fullWidth
+                  required
+                  autoComplete="off"
+                  {...register("email", {
+                    required: true,
+                  })}
+                />
+                <TextField
+                  className={classNames.passwordInput}
+                  type={checked ? "text" : "password"}
+                  variant="filled"
+                  size="small"
+                  label="Password"
+                  required
+                  fullWidth
+                  autoComplete="off"
+                  {...register("password", {
+                    required: true,
+                  })}
+                />
+                <p className={classNames.showPwd}>Show password</p>
+                <Switch
+                  size="small"
+                  className={classNames.switch}
+                  checked={checked}
+                  onChange={(
+                    _: ChangeEvent<HTMLInputElement>,
+                    checked: boolean
+                  ): void => setChecked(checked)}
+                />
+                <Divider className={classNames.divider} />
+                <div className={classNames.buttons}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    variantType="primary"
+                  >
+                    {loading ? (
+                      <Loader
+                        type="Oval"
+                        color="#002233"
+                        height="24"
+                        width="42"
+                      />
+                    ) : (
+                      "Sign Up"
+                    )}
+                  </Button>
+                  <Button
+                    variantType="secondary"
+                    type="button"
+                    variant="outlined"
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+              <p className={classNames.signUpLink}>
+                Already have an account? <Link to="/login">Log In</Link>
+              </p>
+            </div>
+          );
+        }}
+      </UserContext.Consumer>
+    </motion.div>
   );
 };
 
