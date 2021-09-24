56a60e0af954cef57082982f879f34ec7f717139
Rugved Tipare <rugved.tipare25@gmail.com>
2021-09-24 22:14:39 +0530

    Completed the Settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,15 @@
-import { Button, TextField } from "@material-ui/core";
+import { Divider, TextField } from "@material-ui/core";
 import { useSnackbar } from "notistack";
 import { ChangeEvent, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import Loader from "react-loader-spinner";
 import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 import { supabase } from "../db/supabaseClient";
+import { UserActivityInputInterface } from "../db/userActivity";
 import UserContext from "../hooks/userContext";
 import SettingsStyles from "../styles/Settings";
+import { Button } from "./Button";
 
 const Settings = (): JSX.Element => {
   const supabaseUser = supabase.auth.user();
@@ -41,38 +45,96 @@ const Settings = (): JSX.Element => {
     }
   };
 
+  const { reset, handleSubmit, control } =
+    useForm<UserActivityInputInterface>();
+
   return (
     <UserContext.Consumer>
       {(): JSX.Element => {
         return (
           <div className={classNames.parent}>
-            <TextField
-              size="small"
-              label="Change Email"
-              variant="filled"
-              type="email"
-              value={email}
-              onChange={(e: ChangeEvent<HTMLTextAreaElement>): void =>
-                setEmail(e.target.value)
-              }
-            />
-            <TextField
-              size="small"
-              label="Change Password"
-              variant="filled"
-              type="password"
-              value={password}
-              onChange={(e: ChangeEvent<HTMLTextAreaElement>): void =>
-                setPassword(e.target.value)
-              }
-            />
-            <Button variant="contained" onClick={changeSettings}>
-              {loading ? (
-                <Loader type="Oval" color="#002233" height="24" width="42" />
-              ) : (
-                "Save Changes"
-              )}
-            </Button>
+            <h1 className={classNames.heading}>Settings</h1>
+            <Divider className={classNames.divider} />
+            <form
+              className={classNames.form}
+              onSubmit={handleSubmit(changeSettings)}
+            >
+              <Controller
+                control={control}
+                name="email"
+                defaultValue={email}
+                render={({ field: { onChange, value } }): JSX.Element => (
+                  <TextField
+                    className={classNames.emailInput}
+                    type="email"
+                    variant="filled"
+                    size="medium"
+                    label="Update Email"
+                    autoComplete="off"
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                    required
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="password"
+                defaultValue=""
+                rules={{
+                  required: true,
+                }}
+                render={({ field: { value, onChange } }): JSX.Element => (
+                  <TextField
+                    className={classNames.passwordInput}
+                    type="password"
+                    variant="filled"
+                    size="medium"
+                    label="Update Password"
+                    autoComplete="off"
+                    onChange={onChange}
+                    value={value}
+                    required
+                    fullWidth
+                  />
+                )}
+              />
+              <Divider className={classNames.buttonDivider} />
+              <div className={classNames.buttons}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  variantType="primary"
+                  size="large"
+                >
+                  {loading ? (
+                    <Loader
+                      type="Oval"
+                      color="#002233"
+                      height="24"
+                      width="42"
+                    />
+                  ) : (
+                    "Update"
+                  )}
+                </Button>
+                <Button
+                  variantType="secondary"
+                  type="button"
+                  variant="outlined"
+                  onClick={(): void =>
+                    reset({
+                      email: "",
+                      password: "",
+                    })
+                  }
+                  size="large"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
           </div>
         );
       }}
--- src/styles/Settings.ts
+++ src/styles/Settings.ts
@@ -6,13 +6,62 @@ const SettingsStyles = makeStyles((theme: Theme) =>
     parent: {
       display: "flex",
       alignItems: "center",
-      justifyContent: "center",
+      justifyContent: "flex-start",
       height: "100vh",
-      width: "100%",
-      marginTop: "50px",
-      flexDirection: "column",
-      gap: "50px",
       backgroundColor: "#002233",
+      flexDirection: "column",
+    },
+    form: {
+      display: "flex",
+      alignItems: "self-start",
+      justifyContent: "center",
+      flexDirection: "column",
+      width: "80%",
+      maxWidth: "500px",
+    },
+    emailInput: {
+      marginBottom: "30px",
+    },
+    passwordInput: {
+      marginBottom: "20px",
+    },
+    buttons: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      width: "100%",
+      gap: "20px",
+    },
+    heading: {
+      marginTop: "100px",
+      width: "80%",
+      maxWidth: "500px",
+      marginBottom: "20px",
+      fontSize: "1.8rem",
+    },
+    divider: {
+      backgroundColor: "gray",
+      width: "80%",
+      maxWidth: "500px",
+      marginBottom: "30px",
+    },
+    buttonDivider: {
+      backgroundColor: "gray",
+      width: "100%",
+      maxWidth: "500px",
+      marginBottom: "30px",
+    },
+    showPwd: {
+      fontSize: "0.8rem",
+    },
+    switch: {
+      marginBottom: "20px",
+    },
+    signUpLink: {
+      fontSize: "0.8rem",
+      marginTop: "30px",
+      width: "80%",
+      maxWidth: "500px",
     },
   })
 );
